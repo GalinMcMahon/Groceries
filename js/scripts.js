a9a69827dbc56745b18e26f5308e48ee711a8969
Galin McMahon and Tyler Stephenson <tyler@>
2017-03-27 13:04:46 -0700

    create base site to list and sort groceries.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 $(function(){
-
-
-  
+  var groceries = [];
+  var userInputs = ["1", "2", "3"];
+  $("#submit").click(function(event) {
+    userInputs.forEach(function(userInput){
+      groceries.push($("#yourGroceries" + userInput).val());
+    });
+    groceries.sort();
+    var uppercase = groceries.map(function(grocery){
+      return grocery.toUpperCase();
+    });
+    uppercase.forEach(function(grocery) {
+        $("#result-list").append("<li>" + grocery + "</li>");
+    });
+    event.preventDefault();
+  });
 });
